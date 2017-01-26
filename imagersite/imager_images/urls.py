734ad4e51054180546ed5b60b27a5aa73dd6bd9d
Amos Boldor <amosboldor@gmail.com>
2017-01-25 17:53:23 -0800

    turn album and albums into classbased view

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -1,11 +1,11 @@
 """Images urls."""
 from django.conf.urls import url
-from .views import PhotosView, single_album, all_albums, library, PhotoView
+from .views import PhotosView, AlbumView, AlbumsView, library, PhotoView
 
 urlpatterns = [
     url(r'^photos/(?P<photo_id>\d+)', PhotoView.as_view(), name='public_profile'),
     url(r'^photos/$', PhotosView.as_view(), name='private_profile'),  # display all of the public photos that have been uploaded
-    url(r'^albums/(?P<album_id>\d+)', single_album, name='single_album'),  # display a single selected album
-    url(r'^albums/?', all_albums, name='all_albums'),
+    url(r'^albums/(?P<album_id>\d+)', AlbumView.as_view(), name='AlbumView'),  # display a single selected album
+    url(r'^albums/?', AlbumsView.as_view(), name='AlbumsView'),
     url(r'^library$', library, name='library'),
 ]
--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -21,13 +21,13 @@ class PhotoView(ListView):
 
 
 class PhotosView(ListView):
-    """Photo View."""
+    """Photos View."""
 
     model = Photo
     template_name = "imager_images/photos.html"
 
     def get_context_data(self):
-        """Return photo."""
+        """Return photos."""
         public_photos = []
         photos = Photo.objects.all()
         for photo in photos:
@@ -36,30 +36,34 @@ class PhotosView(ListView):
         return {"photos": public_photos}
 
 
-def single_album(request, album_id):
-    """Render a specific album."""
-    album = Album.objects.get(id=album_id)
-    if album.published != 'private' or album.user.username == request.user.username:
-        return render(
-            request,
-            'imager_images/album.html',
-            {'album': album}
-        )
-    return HttpResponse('Unauthorized', status=401)
+class AlbumView(ListView):
+    """Album View."""
+
+    model = Album
+    template_name = "imager_images/album.html"
+
+    def get_context_data(self):
+        """Return album."""
+        album = Album.objects.get(id=self.kwargs['album_id'])
+        if album.published != 'private' or album.user.username == self.request.user.username:
+            return {'album': album}
+        return HttpResponse('Unauthorized', status=401)
 
 
-def all_albums(request):
-    """Render all public albums."""
-    public_albums = []
-    albums = Album.objects.all()
-    for album in albums:
-        if album.published != 'private' or album.user.username == request.user.username:
-            public_albums.append(album)
-    return render(
-        request,
-        'imager_images/albums.html',
-        {'albums': public_albums}
-    )
+class AlbumsView(ListView):
+    """Albums View."""
+
+    model = Album
+    template_name = "imager_images/albums.html"
+
+    def get_context_data(self):
+        """Return albums."""
+        public_albums = []
+        albums = Album.objects.all()
+        for album in albums:
+            if album.published != 'private' or album.user.username == self.request.user.username:
+                public_albums.append(album)
+        return {'albums': public_albums}
 
 
 @login_required(login_url='/accounts/login/')
