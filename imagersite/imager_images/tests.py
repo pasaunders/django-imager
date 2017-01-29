fa770835437a06a99fe6f55e4f48d7e724cc3cf7
Amos Boldor <amosboldor@gmail.com>
2017-01-29 12:39:16 -0800

    add test image and made more tests for image model

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -1,7 +1,7 @@
 """Test the imager_images app."""
 from django.test import TestCase, Client, RequestFactory
 from django.contrib.auth.models import User
-from django.db import models
+from django.core.files.uploadedfile import SimpleUploadedFile
 from imager_images.models import Photo, Album
 import factory
 
@@ -28,6 +28,7 @@ class PhotoFactory(factory.django.DjangoModelFactory):
 
         model = Photo
 
+    user = factory.SubFactory(UserFactory)
     title = factory.Sequence(lambda n: "Photo number {}".format(n))
     description = factory.LazyAttribute(lambda a: '{} is a photo'.format(a.title))
 
@@ -62,7 +63,7 @@ class PhotoTestCase(TestCase):
 
     def test_photo_made_when_saved(self):
         """Test photos are added to the database."""
-        self.assertTrue(Photo.objects.count() == 1)
+        self.assertTrue(Photo.objects.count() == 20)
 
     def test_photo_associated_with_user(self):
         """Test that a photo is attached to a user."""
@@ -72,6 +73,69 @@ class PhotoTestCase(TestCase):
 
     def test_photo_has_str(self):
         """Test photo model includes string method."""
+
+    def test_image_title(self):
+        """Test that the image has a title."""
+        self.assertTrue("Photo" in Photo.objects.first().title)
+
+    def test_image_has_description(self):
+        """Test that the Photo description field can be assigned."""
+        image = Photo.objects.first()
+        description = "This is a test of description field."
+        image.description = description
+        image.save()
+        self.assertTrue(Photo.objects.first().description == description)
+
+    def test_image_has_published(self):
+        """Test the image published field."""
+        image = Photo.objects.first()
+        image.published = 'public'
+        image.save()
+        self.assertTrue(Photo.objects.first().published == "public")
+
+    def test_user_has_image(self):
+        """Test that the user has the image."""
+        image = Photo.objects.first()
+        user = User.objects.first()
+        self.assertTrue(user.photos.count() == 0)
+        image.user = user
+        image.save()
+        self.assertTrue(user.photos.count() == 1)
+
+    def test_two_images_have_user(self):
+        """Test two images have the same user."""
+        image1 = Photo.objects.all()[0]
+        image2 = Photo.objects.all()[1]
+        user = User.objects.first()
+        image1.user = user
+        image2.user = user
+        image1.save()
+        image2.save()
+        self.assertTrue(image1.user == user)
+        self.assertTrue(image2.user == user)
+
+    def test_user_has_two_images(self):
+        """Test that user has two image."""
+        image1 = Photo.objects.all()[0]
+        image2 = Photo.objects.all()[1]
+        user = User.objects.first()
+        image1.user = user
+        image2.user = user
+        image1.save()
+        image2.save()
+        self.assertTrue(user.photos.count() == 2)
+
+    def test_user_has_photo_uploaded(self):
+        """Test user has photo uploaded."""
+        photo = self.photos[4]
+        self.assertTrue(photo.image.name is None)
+        image = SimpleUploadedFile(
+            name='test_image.jpg',
+            content=open('imager_images/test_img.jpg', 'rb').read(),
+            content_type='image/jpeg'
+        )
+        photo.image = image
+        self.assertTrue(photo.image.name is not None)
 
 
 class AlbumTestCase(TestCase):
