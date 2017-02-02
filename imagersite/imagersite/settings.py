18c17131a6dadfad41ac262a9674f39c556673d6
Amos Boldor <amosboldor@gmail.com>
2017-02-01 18:00:41 -0800

    update settings

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -132,16 +132,16 @@ STATIC_URL = '/static/'
 
 # Registration Settings
 ACCOUNT_ACTIVATION_DAYS = 3
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = '' # email address goes in the string
+EMAIL_HOST_USER = 'djangoimagersite@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get['EMAIL_PASSWORD']
 
 # Login/out settings
 LOGIN_REDIRECT_URL='/profile/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
 MEDIA_URL = "/media/"
-THUMBNAIL_DEBUG = True
+THUMBNAIL_DEBUG = False
