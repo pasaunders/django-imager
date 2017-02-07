f43adadd24ad4c055ae5fad20ddf9d27db30066a
Amos Boldor <amosboldor@gmail.com>
2017-02-06 20:00:10 -0800

    add serializer, view and url for showing all the photos for a given user

--- imagersite/imager_api/views.py
+++ imagersite/imager_api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+"""Views for restfull api."""
+from imager_images.models import Photo
+from rest_framework import viewsets
+from imager_api.serializers import PhotosSerializer
 
-# Create your views here.
+
+class PhotoViewSet(viewsets.ReadOnlyModelViewSet):
+    """Provides a list of all photos belonging to a user."""
+
+    serializer_class = PhotosSerializer
+
+    def get_queryset(self):
+        """Get photos owned by current user."""
+        return Photo.objects.filter(user=self.request.user)
--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -21,6 +21,11 @@ from django.contrib import (
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from rest_framework.routers import DefaultRouter
+from imager_api import views
+
+router = DefaultRouter()
+router.register(r'photos', views.PhotoViewSet, base_name="photos")
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -29,5 +34,6 @@ urlpatterns = [
     url(r'^login/', auth.views.login, name='login'),
     url(r'^logout/', auth.views.logout, {'next_page': '/'}, name='logout'),
     url(r'^profile/', include('imager_profile.urls')),
-    url(r'^images/', include('imager_images.urls'))
+    url(r'^images/', include('imager_images.urls')),
+    url(r'^api/v1/', include(router.urls))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
