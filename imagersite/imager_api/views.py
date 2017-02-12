1f622a8b51b0127801270ab3580d139c5a23d6aa
Amos Boldor <amosboldor@gmail.com>
2017-02-12 13:04:07 -0800

    add permissions

--- imagersite/imager_api/views.py
+++ imagersite/imager_api/views.py
@@ -1,13 +1,15 @@
 """Views for restfull api."""
 from imager_images.models import Photo
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from imager_api.serializers import PhotosSerializer
+from imager_api.permissions import Owner
 
 
 class PhotoViewSet(viewsets.ReadOnlyModelViewSet):
     """Provides a list of all photos belonging to a user."""
 
     serializer_class = PhotosSerializer
+    permission_classes = (permissions.IsAuthenticated, Owner)
 
     def get_queryset(self):
         """Get photos owned by current user."""
